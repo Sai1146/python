380bff8451f5767e935302cb59688ef893037f86
Sai Pokuri <146319447+Sai1146@users.noreply.github.com>
2024-05-22 11:59:34 +0530

    python.py

--- python.py
+++ python.py
@@ -1,3 +1,4 @@
+# 2nd question answers
 def cube(a):
   c = []
   for i in range(1,a+1):
@@ -5,4 +6,22 @@ def cube(a):
   return c
 a = int(input())
 print(cube(a))
-  
+
+
+# 1st question anser 
+# option 1
+a = list(map(str, input().split()))
+for i in range(len(a)):
+    if (i%2!=0):
+      ans = a[i]
+      print("odd index position value:",ans)
+
+#option 2
+def odd(a):
+  for i in range(len(a)):
+    if (i%2!=0):
+      ans = a[i]
+      return ans
+      
+a = list(map(str, input().split()))
+print("odd index position value:",odd(a))
